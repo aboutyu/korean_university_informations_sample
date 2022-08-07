65fdd4d277bd65d63be937462d7449389dafb5b2
yutaehun <aboutyu@gmail.com>
2022-08-07 10:23:17 +0900

    ios, 안드로이드 프로젝트 생성

---
+++ KoreanUniversityIOSApp/KoreanUniversityIOSApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KoreanUniversityIOSApp
+//
+//  Created by 유태훈 on 07/08/2022.
+//  Copyright © 2022 유태훈. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
